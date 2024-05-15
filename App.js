7a661913fab18f551eda3134cb4e9db419529178
Ajaxshadow <dudufaga2016@gmail.com>
2024-05-15 21:19:35 +0100

    so far

--- App.js
+++ App.js
@@ -8,17 +8,23 @@ import Upload from './pages/Upload';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
 
-const Stack = createNativeStackNavigator();
+function StackNavigator(){
+const Stack = createNativeStackNavigator()
+
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home}/>
+      <Stack.Screen options={{headerShown: false}} name='Signup' component={SignUp}/>
+      <Stack.Screen options={{headerShown: false}} name='Login' component={LogIn}/>
+      <Stack.Screen name='Upload' component={Upload}/>
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen options={{headerShown: false}} name='Signup' component={SignUp}/>
-      <Stack.Screen name='Home' component={Home}/>
-      <Stack.Screen options={{headerShown: false}} name='Login' component={LogIn}/>
-      <Stack.Screen name='Upload' component={Upload}/>
-    </Stack.Navigator>
+    <StackNavigator/>
     </NavigationContainer>
     
   );
--- pages/Home.js
+++ pages/Home.js
@@ -1,34 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, StatusBar, ScrollView, Image, Modal } from 'react-native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
-import Upload from './Upload'
+import { launchImageLibrary } from 'react-native-image-picker';
+import { useState } from 'react';
 
-const Home=({navigation})=>{
+const Home = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaView>
-      <StatusBar/>
-      <View style={{ marginTop: 5, flexDirection: 'row', justifyContent: 'space-between', position: 'relative'}}>
-        
-        <Ionicons 
-        onPress={()=>navigation.navigate(Upload)} 
-        name='menu'
-        style={{fontSize: 30}}/>
+      <ScrollView>
+        <StatusBar />
+        <View style={{ marginTop: 20, marginLeft: 15 }}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Welcome User</Text>
+          <Text>Good day</Text>
 
-        <Text style={{fontSize: 20}}>Photoruum</Text>
-
-        <FontAwesome name='id-card-o' style={{fontSize: 30}}/>
-
-      </View>
-
-      <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 50, marginRight: 80, marginLeft:80, backgroundColor:'purple', borderRadius:20, height: 200}}>
-         <Text style={{fontSize: 30}}>Upload Photos Here</Text>
-      </View>
+          {/* <Modal>
+          <View style={{height: 400, width: '100%'}}>
+            <Image style={{height: 400, width: '100%'}} source={{uri:selectImage}}/>
+          </View>
+          </Modal> */}
 
 
+          <TouchableOpacity onPress={UploadImage} style={styles.container}>
+            <Ionicons name='cloud-upload-outline' size={50} />
+            <Text>Tap to upload images</Text>
+          </TouchableOpacity>
+
+          <Text style={{ marginTop: 15, fontWeight: 'bold' }}>Recent uploads</Text>
+          <ScrollView style={{ marginTop: 15 }}>
+            <View>
+              <Text>Hello</Text>
+              <Text>World</Text>
+            </View>
+          </ScrollView>
+
+        </View>
+
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#E2E2E2",
+    borderRadius: 10,
+    height: 300,
+    width: 300,
+    borderStyle: 'dashed'
+  }
+})
