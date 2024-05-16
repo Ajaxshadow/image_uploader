e411f67d78008f7b8d9eeb1b06e3e7caa50b6556
Ajaxshadow <dudufaga2016@gmail.com>
2024-05-16 23:01:31 +0100

    update

--- App.js
+++ App.js
@@ -4,28 +4,18 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './pages/Home';
-import Upload from './pages/Upload';
-import SignUp from './pages/SignUp';
+import SignUp from './Components/Uploading';
 import LogIn from './pages/LogIn';
+import ProgressBar from './Components/ProgressBar';
+import Upload from './Components/Uploading';
 
 function StackNavigator(){
 const Stack = createNativeStackNavigator()
-
-  return(
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home}/>
-      <Stack.Screen options={{headerShown: false}} name='Signup' component={SignUp}/>
-      <Stack.Screen options={{headerShown: false}} name='Login' component={LogIn}/>
-      <Stack.Screen name='Upload' component={Upload}/>
-    </Stack.Navigator>
-  )
 }
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <StackNavigator/>
-    </NavigationContainer>
-    
+    <Home/>
   );
 }
+
--- pages/Home.js
+++ pages/Home.js
@@ -1,43 +1,133 @@
-import { Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, StatusBar, ScrollView, Image, Modal } from 'react-native';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-import { launchImageLibrary } from 'react-native-image-picker';
-import { useState } from 'react';
+import {
+  Platform, SafeAreaView, StyleSheet,
+  Text, TouchableOpacity, View,
+  StatusBar, ScrollView, Image,
+  Modal, FlatList
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { db, storage } from "../firebaseConfig";
+import ProgressBar from '../Components/ProgressBar';
+
+
+
 
 const Home = ({ navigation }) => {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "files"), (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          console.log("New file", change.doc.data());
+          setFiles((prevFiles) => [...prevFiles, change.doc.data()]);
+        }
+      });
+    });
+    return () => unsubscribe();
+  }, []);
+
+
+  async function pickImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      allowsEditing: true,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      await uploadImage(result.assets[0].uri, "image");
+    }
+  }
+
+  async function uploadImage(uri, fileType) {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const storageRef = ref(storage, "Pics/" + new Date().getTime());
+    const uploadTask = uploadBytesResumable(storageRef, blob);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        setProgress(progress.toFixed());
+      },
+      (error) => {
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          console.log("File available at", downloadURL);
+          await saveRecord(fileType, downloadURL, new Date().toISOString());
+          setImage("");
+        });
+      }
+    );
+  }
+
+  async function saveRecord(fileType, url, createdAt) {
+    try {
+      const docRef = await addDoc(collection(db, "files"), {
+        fileType,
+        url,
+        createdAt,
+      });
+      console.log("document saved correctly", docRef.id);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
 
   return (
     <SafeAreaView>
-      <ScrollView>
-        <StatusBar />
-        <View style={{ marginTop: 20, marginLeft: 15 }}>
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Welcome User</Text>
-          <Text>Good day</Text>
+      <StatusBar />
+      <View style={{ marginTop: 20, marginLeft: 15 }}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Welcome User</Text>
+        <Text>Good day</Text>
+      </View>
 
-          {/* <Modal>
-          <View style={{height: 400, width: '100%'}}>
-            <Image style={{height: 400, width: '100%'}} source={{uri:selectImage}}/>
-          </View>
-          </Modal> */}
+      <View style={{ alignItems: 'center', marginTop: 30 }}>
+        <FlatList
+          data={files}
+          keyExtractor={(item) => item.url}
+          renderItem={({ item }) => {
+            if (item.fileType === "image") {
+              return (
+                <Image
+                  source={{ uri: item.url }}
+                  style={{ width: "34%", height: 100 }}
+                />
+              );
+            }
+          }}
+          numColumns={3}
+          contentContainerStyle={{ gap: 2 }}
+          columnWrapperStyle={{ gap: 2 }}
+        />
+        <TouchableOpacity onPress={pickImage} style={styles.container}>
+          <Ionicons name='cloud-upload-outline' size={50} />
+          <Text>Tap to upload images</Text>
+        </TouchableOpacity>
+      </View>
 
+      <View style={{marginTop: 20, alignItems: 'center', justifyContent: 'center'}}>
+      {image && <ProgressBar image={image} progress={progress} />}
+      </View>
 
-          <TouchableOpacity onPress={UploadImage} style={styles.container}>
-            <Ionicons name='cloud-upload-outline' size={50} />
-            <Text>Tap to upload images</Text>
-          </TouchableOpacity>
+      <View style={{ alignItems: 'center' }}>
+        <Image source={require('../assets/Photoruum-black-text.jpg')}
+          style={{ height: 100, width: 300 }} />
+      </View>
 
-          <Text style={{ marginTop: 15, fontWeight: 'bold' }}>Recent uploads</Text>
-          <ScrollView style={{ marginTop: 15 }}>
-            <View>
-              <Text>Hello</Text>
-              <Text>World</Text>
-            </View>
-          </ScrollView>
-
-        </View>
-
-      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -53,6 +143,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     height: 300,
     width: 300,
-    borderStyle: 'dashed'
+    borderStyle: 'dashed',
+    borderWidth: 1
+  },
+  uploadbtn: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 15
   }
 })
